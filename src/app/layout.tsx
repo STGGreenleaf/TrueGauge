71a89e4896f1109ff2f0210b348863c182b4694a
STGGreenleaf <collingreenleaf@gmail.com>
2026-01-08 13:48:57 -0700

    Reduce top padding to pt-8 (32px)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased pt-10`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased pt-8`}
       >
         {children}
       </body>
