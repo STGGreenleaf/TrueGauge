c31404c06f6193cb1b59876f0c600d2d617b86fb
STGGreenleaf <collingreenleaf@gmail.com>
2026-01-11 22:03:33 -0700

    Add Google Analytics tracking (G-3M34WR1NEE)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import prisma from "@/lib/db";
+import { GoogleAnalytics } from "@/components/GoogleAnalytics";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -98,6 +99,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased pt-8 bg-black`}
       >
+        <GoogleAnalytics />
         {children}
       </body>
     </html>
