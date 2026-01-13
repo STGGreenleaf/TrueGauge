e8c2a7fb03d3242ddb5b29d06139de97201d8c6d
STGGreenleaf <collingreenleaf@gmail.com>
2026-01-13 10:24:20 -0700

    Silence Vercel instrumentation deprecation warnings in console

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -7,6 +7,18 @@ const GA_MEASUREMENT_ID = 'G-3M34WR1NEE';
 export function GoogleAnalytics() {
   return (
     <>
+      {/* Silence Vercel instrumentation deprecation warnings */}
+      <Script id="console-filter" strategy="beforeInteractive">
+        {`
+          (function() {
+            const originalWarn = console.warn;
+            console.warn = function(...args) {
+              if (args[0] && typeof args[0] === 'string' && args[0].includes('[DEPRECATED]')) return;
+              originalWarn.apply(console, args);
+            };
+          })();
+        `}
+      </Script>
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
         strategy="afterInteractive"
